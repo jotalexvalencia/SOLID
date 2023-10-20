c10e7f6d0c7c32db5e51fdf868a433c926146147
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-10-20 14:18:35 -0500

    Agregar archivos de proyecto.

---
+++ SingleResponsaability1/CEmpleado.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SingleResponsibility1
+{
+    class CEmpleado
+    {
+        private string nombre;
+        private string puesto;
+        private int edad;
+        private double sueldo;
+
+        public CEmpleado(string pNombre, string pPuesto, int pEdad, double pSueldo)
+        {
+            nombre = pNombre;
+            puesto = pPuesto;
+            edad = pEdad;
+            sueldo = pSueldo;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0},{1},{2},{3}", nombre, puesto, edad, sueldo);
+        }
+
+        // Aqui van otros metodos propios de empleado
+        // AsignarHorario
+        // ProyectoActual
+
+        // Ahora colocamos algo que no es responsabilidad del empleado
+        // Tenemos DOS razones para cambiar la clase
+        // Cuando cambia algo del empleado y
+        // cuando cambia algo relacionado con los impuestos
+
+        public double CalcularImpuesto()
+        {
+            return sueldo * 0.35;
+        }
+
+        public void PagarImpuesto()
+        {
+            double imp = CalcularImpuesto();
+            Console.WriteLine("Se pago {0} en impuestos por parte de {1}", imp, nombre);
+        }
+    }
+}
---
+++ SingleResponsaability1/Program.cs
@@ -0,0 +1,19 @@
+﻿using SingleResponsibility1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsaability1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CEmpleado empleado = new CEmpleado("Alex", "Ingeniero Desarrollo de Software", 48, 2500000);
+            Console.WriteLine(empleado);
+            empleado.PagarImpuesto();
+        }
+    }
+}
