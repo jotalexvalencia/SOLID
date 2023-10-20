6ad128b3e7524f21cf0b19655f545c6d4d4cde22
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-10-20 14:32:10 -0500

    Principio Abierto cerrado sin aplicar

---
+++ OpenClosed/CTienda.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OpenClosed
+{
+    class CTienda
+    {
+        private List<CProducto> productos;
+
+        public CTienda(List<CProducto> pProductos)
+        {
+            productos = pProductos;
+        }
+
+        public void calcularInventario()
+        {
+            double total = 0;
+
+            //foreach (CProducto producto in productos)
+            //{
+            //    Console.WriteLine(producto);
+            //    total += producto.Precio;
+            //}
+
+            // Cambio
+            foreach (CProducto producto in productos)
+            {
+                if (producto.Categoria == 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    producto.Precio *= 0.8;
+                    Console.WriteLine(producto);
+                    total += producto.Precio;
+                }
+                if (producto.Categoria == 2)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    producto.Precio *= 1.2;
+                    Console.WriteLine(producto);
+                    total += producto.Precio;
+                }
+            }
+
+            Console.WriteLine("El total en inventario es {0}", total);
+        }
+    }
+}
---
+++ OpenClosed/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace OpenClosed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<CProducto> misProductos = new List<CProducto>
+            {
+                new CProducto("Papas",1,12.5),
+                new CProducto("Analgesico",2,23.40),
+                new CProducto("Tomate",1,30),
+                new CProducto("Manzana",1,25),
+                new CProducto("Antibiotico",2,89),
+                new CProducto("Antiacido",2,43)
+            };
+
+            CTienda miTienda = new CTienda(misProductos);
+            miTienda.calcularInventario();
+        }
+    }
+}
